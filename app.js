84898185e78510c42e6c6cc5284a28cc1bade2ec
Don Burks <don@donburks.com>
2018-03-20 15:40:47 -0700

    Converted syntax to ES6 for demonstration purposes.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser')
 
-var app = express();
+const app = express();
 var counter = 0;
 
 app.use(express.static('public'));
@@ -9,31 +9,23 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs');
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   counter++;
   req.counter = counter;
   next(); 
 });
 
-app.get('/', function(req, res) {
-  res.end("You requested the root route, and the counter is set to: " + req.counter);
-});
+app.get('/', (req, res) => res.end("You requested the root route, and the counter is set to: " + req.counter));
 
-app.get('/index.html', function(req, res) {
-  res.render('floop', {counter: req.counter});
-});
+app.get('/index.html', (req, res) => res.render('floop', {counter: req.counter}));
 
-app.get('/loadForm', function(req, res) {
-  res.render("myForm");
-});
+app.get('/loadForm', (req, res) => res.render("myForm"));
 
-app.post('/sendForm', function(req, res) {
+app.post('/sendForm', (req, res) => {
   var num1 = Number(req.body.num_one);
   var num2 = Number(req.body.num_two);
   counter = counter + num1 + num2;
   res.redirect('/index.html');
 });
 
-app.listen(3000, function() {
-  console.log("Express app listening on port 3000");
-});
+app.listen(3000, () => console.log("Express app listening on port 3000"));
